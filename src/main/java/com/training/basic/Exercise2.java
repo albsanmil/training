378eaf5c5900d84484c8c8464d84176fb461fd4f
albsanmil <albsanmil@gmail.com>
2018-04-18 13:32:30 -0400

    Add Exercise 2

---
+++ src/main/java/com/training/basic/Exercise2.java
@@ -0,0 +1,13 @@
+package com.training.basic;
+
+public class Exercise2 {
+
+    public static void main(String[] args) {
+        System.out.println(addition(74, 36));
+    }
+
+    private static int addition(int firstNumber, int secondNumber) {
+        return firstNumber + secondNumber;
+    }
+
+}
