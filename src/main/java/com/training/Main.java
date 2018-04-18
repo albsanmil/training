f9cc1fc1a9ea8b760ec887738c777f504f4f1609
albsanmil <albsanmil@gmail.com>
2018-04-18 13:14:55 -0400

    Add Exercise 1, final version

--- src/main/java/com/training/Main.java
+++ src/main/java/com/training/Main.java
@@ -1,8 +1,25 @@
 package com.training;
 
+import com.training.model.Car;
+
 public class Main {
 
     public static void main(String[] args) {
         System.out.println("Hello world!!!");
+
+        System.out.println("Number of cars " + Car.getNumberOfCars());
+
+        Car ferrari = new Car(4, "blue");
+//        ferrari.setColour("blue");
+        System.out.println("Ferrari - wheels: " + ferrari.getNumWheels() + ", colour: " + ferrari.getColour());
+
+        System.out.println("Number of cars " + Car.getNumberOfCars());
+
+        Car fiat = new Car(4, "yellow");
+        System.out.println("Fiat - wheels: " + fiat.getNumWheels() + ", colour: " + fiat.getColour());
+
+        System.out.println("Number of cars " + Car.getNumberOfCars());
+
+
     }
 }
--- src/main/java/com/training/model/Car.java
+++ src/main/java/com/training/model/Car.java
@@ -1,4 +1,101 @@
 package com.training.model;
 
 public class Car {
+
+    private static int numberOfCars = 0;
+
+    // Size 8 bits
+    private byte numSeats = 5;
+    // Size 16 bits
+    private short numCarpets = 4;
+    // Size 32 bits
+    private int numWheels = 4;
+    // Size 64 bits
+    private long numScrews = 1432897423L;
+
+    // Use '' for characters
+    private char model = 'U';
+
+    /* Use "" for Strings */
+    private String colour = "red";
+
+    // Size 32 bits
+    private float price = 1000.50f;
+    // Size 64 bits
+    private double temperatureEngine = 90.45;
+
+
+    public Car(int numWheels, String colour) {
+        this.numWheels = numWheels;
+        this.colour = colour;
+        ++numberOfCars;
+    }
+
+    public byte getNumSeats() {
+        return numSeats;
+    }
+
+    public void setNumSeats(byte numSeats) {
+        this.numSeats = numSeats;
+    }
+
+    public short getNumCarpets() {
+        return numCarpets;
+    }
+
+    public void setNumCarpets(short numCarpets) {
+        this.numCarpets = numCarpets;
+    }
+
+    public int getNumWheels() {
+        return numWheels;
+    }
+
+    public void setNumWheels(int numWheels) {
+        this.numWheels = numWheels;
+    }
+
+    public long getNumScrews() {
+        return numScrews;
+    }
+
+    public void setNumScrews(long numScrews) {
+        this.numScrews = numScrews;
+    }
+
+    public char getModel() {
+        return model;
+    }
+
+    public void setModel(char model) {
+        this.model = model;
+    }
+
+    public String getColour() {
+        return colour;
+    }
+
+    public void setColour(String colour) {
+        this.colour = colour;
+    }
+
+    public float getPrice() {
+        return price;
+    }
+
+    public void setPrice(float price) {
+        this.price = price;
+    }
+
+    public double getTemperatureEngine() {
+        return temperatureEngine;
+    }
+
+    public void setTemperatureEngine(double temperatureEngine) {
+        this.temperatureEngine = temperatureEngine;
+    }
+
+    public static int getNumberOfCars() {
+        return numberOfCars;
+    }
 }
