9baa8fdbb743ef8aad4685d4f07a411e1a207ebf
albsanmil <albsanmil@gmail.com>
2018-04-24 05:33:28 -0400

    Exercise 5 Arrays

--- src/main/java/com/training/basic/Exercise12.java
+++ src/main/java/com/training/basic/Exercise12.java
@@ -8,16 +8,16 @@ public class Exercise12 {
 
         Scanner introNumber = new Scanner(System.in);
 
-        int firstNumber = 5;
-        int secondNumber = 5;
-        int thirdNumber = 5;
+        int firstNumber;
+        int secondNumber;
+        int thirdNumber;
 
-//        System.out.print("Enter the first number: ");
-//        firstNumber = introNumber.nextInt();
-//        System.out.print("Enter the second number: ");
-//        secondNumber = introNumber.nextInt();
-//        System.out.print("Enter the third number: ");
-//        thirdNumber = introNumber.nextInt();
+        System.out.print("Enter the first number: ");
+        firstNumber = introNumber.nextInt();
+        System.out.print("Enter the second number: ");
+        secondNumber = introNumber.nextInt();
+        System.out.print("Enter the third number: ");
+        thirdNumber = introNumber.nextInt();
         System.out.println("____________________________________________");
         System.out.println(firstNumber);
         System.out.println("The average of the three numbers is: " + average(firstNumber, secondNumber, thirdNumber));
@@ -27,10 +27,8 @@ public class Exercise12 {
 
     }
 
-    private static int average(int a, int secondNumber, int thirdNumber) {
+    private static int average(int firstNumber, int secondNumber, int thirdNumber) {
 
-        a = 6;
-
-        return (a + secondNumber + thirdNumber) / 3;
+        return (firstNumber + secondNumber + thirdNumber) / 3;
     }
 }
