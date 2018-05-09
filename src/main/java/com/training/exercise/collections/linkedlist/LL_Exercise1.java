13918bf9fb9c6589b1dba0644b2700c2a1ad26f5
albsanmil <albsanmil@gmail.com>
2018-05-09 13:29:50 -0400

    Initiation of exercises with linkedlist

---
+++ src/main/java/com/training/exercise/collections/linkedlist/LL_Exercise1.java
@@ -0,0 +1,5 @@
+package com.training.exercise.collections.linkedlist;
+
+//  1. Write a Java program to append the specified element to the end of a linked list.
+public class LL_Exercise1 {
+}
---
+++ src/main/java/com/training/exercise/collections/linkedlist/LL_Exercise4.java
@@ -0,0 +1,5 @@
+package com.training.exercise.collections.linkedlist;
+
+//  4. Write a Java program to iterate a linked list in reverse order.
+public class LL_Exercise4 {
+}
