7fbe260f5949b3c62d0407d012b25d15dcf560bb
albsanmil <albsanmil@gmail.com>
2018-05-09 13:30:18 -0400

    Initiation of exercises with TreeSet

---
+++ src/main/java/com/training/exercise/collections/treeset/TS_Exercise1.java
@@ -0,0 +1,13 @@
+package com.training.exercise.collections.treeset;
+
+import java.util.Set;
+import java.util.TreeSet;
+
+// 1. Write a Java program to create a new tree set, add some colors (string) and print out the tree set.
+public class TS_Exercise1 {
+
+    public static void main(String[] args) {
+        Set<String> colors = new TreeSet<>();
+
+    }
+}
