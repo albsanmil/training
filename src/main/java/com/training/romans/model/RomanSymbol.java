81c531ba4d24f41decb5abf95a7112bfd1fb5898
albsanmil <albsanmil@gmail.com>
2018-05-22 11:25:09 -0400

    Moved roman symbol class and its tests to the model package

---
+++ src/main/java/com/training/romans/model/RomanSymbol.java
@@ -0,0 +1,38 @@
+package com.training.romans.model;
+
+public enum RomanSymbol {
+
+    I('I', 1),
+    V('V', 5),
+    X('X', 10),
+    L('L', 50),
+    C('C', 100),
+    D('D', 500),
+    M('M', 1000);
+
+    private final char character;
+    private final int code;
+
+    RomanSymbol(char character, int code) {
+        this.character = character;
+        this.code = code;
+    }
+
+    public char getCharacter() {
+        return character;
+    }
+
+    public int getCode() {
+        return code;
+    }
+
+    public static RomanSymbol fromCharacter(char character) {
+        for (RomanSymbol romanSymbol : RomanSymbol.values()) {
+            if (romanSymbol.character == character) {
+                return romanSymbol;
+            }
+        }
+
+        throw new IllegalArgumentException("'" + character + "' is not a valid roman symbol");
+    }
+}
