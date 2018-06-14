381a53332989e8382f3e349fd52c7260204fa9ca
albsanmil <albsanmil@gmail.com>
2018-06-14 03:28:40 -0400

    Validator time roman number. nanoTime(), currentTimeMillis().

--- src/test/java/com/training/romans/RomanNumberValidationBenchmark.java
+++ src/test/java/com/training/romans/RomanNumberValidationBenchmark.java
@@ -10,11 +10,39 @@ public class RomanNumberValidationBenchmark {
     public static void main(String[] args) throws Exception {
         Path path = Paths.get(Thread.currentThread().getContextClassLoader().getResource("romans/romanNumbers").toURI());
         Stream<String> lines = Files.lines(path);
-        // System.nanoTime();
+
+        double initialTimeNano;
+        double finalTimeNano;
+        double initialTimeMillis;
+        double finalTimeMillis;
+
+        initialTimeNano = System.nanoTime();
+        initialTimeMillis = System.currentTimeMillis();
+
         lines.forEach(line -> {
             String[] lineParts = line.split("=");
             RomanNumber romanNumber = RomanNumber.valueOf(lineParts[1]);
         });
         lines.close();
+
+        finalTimeNano = System.nanoTime();
+        finalTimeMillis = System.currentTimeMillis();
+
+        double resultNano = (finalTimeNano - initialTimeNano);
+        double resultMillis = (finalTimeMillis - initialTimeMillis);
+
+        System.out.println("Initial time in nano seconds: " + initialTimeNano);
+        System.out.println("Final time in nano seconds: " + finalTimeNano);
+        System.out.println("________________________________________");
+        System.out.println("Result in nano seconds: " + resultNano + " Nano seconds");
+        System.out.println("Result in seconds: " + resultNano / 1000000000 + " Seconds");
+        System.out.println("_____________________________________________________________________________");
+        System.out.println("_____________________________________________________________________________");
+        System.out.println("Initial time in milli seconds: " + initialTimeMillis);
+        System.out.println("Final time in milli seconds: " + finalTimeMillis);
+        System.out.println("________________________________________");
+        System.out.println("Result in milli seconds: " + resultMillis + " Nano seconds");
+        System.out.println("Result in seconds: " + resultMillis / 1000 + " Seconds");
+
     }
 }
